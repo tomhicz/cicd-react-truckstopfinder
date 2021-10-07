e08644a6248d0310f885d39fcc7b7161f7b5a26c
Gabe-Goodman <goodman.g@northeastern.edu>
2021-10-07 21:36:56 +0900

    Component layout, styled components created

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import Map from "./Map";
+import Results from "./Results";
+import Search from "./Search";
+
+export default function App() {
+  //state
+  const [currentView, setCurrentView] = useState();
+
+  //hooks
+  useEffect(() => {}, []);
+
+  //handlers
+
+  return (
+    <div className="app">
+      <Map />
+      {currentView === "Search" ? <Search setCurrentView={setCurrentView} /> : null}
+      {currentView === "Results" ? <Results setCurrentView={setCurrentView} /> : null}
+    </div>
+  );
+}
