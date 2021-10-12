1ff3e88e094c18dff3bb42c52efb7bc96861f72b
Tom Hickz <999194+tomhicz@users.noreply.github.com>
2021-10-12 17:40:58 +0900

    working basic results with fake network

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,14 +3,34 @@ import { ResultWrapper } from "../elements/ResultWrapper";
 
 export default function Result(props) {
   //state
+  const i = props.result;
 
+  const tel = i.ContactMethods.filter((val) => {
+    if (val.Type.Id === 1 && val.Type.IsActive === true) {
+      return true;
+    }
+    return false;
+  });
+  console.log("tel", tel);
   //hooks
 
   //handlers
 
   return (
     <ResultWrapper>
-      {props.result.name} | {props.result.address} | {props.result.tel}
+      <div>
+        <h3>{i.Name}</h3>
+        Exit {i.Site.ExitNumber} {i.Site.DescriptiveAddress} Store #{i.Number}
+      </div>
+      <div>
+        {i.Addresses[0].Address1} {i.Addresses[0].Address2} {i.Addresses[0].City}{" "}
+        {i.Addresses[0].State}
+        {i.Addresses[0].Zip}
+      </div>
+      <hr />
+      <div>
+        {tel[0].Type.Name}: {tel[0].Data}
+      </div>
     </ResultWrapper>
   );
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,25 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ResultsWrapper } from "../elements/ResultsWrapper";
 import Result from "./Result";
+import { query } from "./network";
 
 export default function Results() {
   //state
-  const resArr = [
-    { name: "stop 1", address: "2001 abc st", tel: "0805550001" },
-    { name: "stop 2", address: "2002 abc st", tel: "0805550002" },
-    { name: "stop 3", address: "2003 abc st", tel: "0805550003" },
-  ];
-  const [results, setResults] = useState(resArr);
+  // const resArr = [
+  //   { name: "stop 1", address: "2001 abc st", tel: "0805550001" },
+  //   { name: "stop 2", address: "2002 abc st", tel: "0805550002" },
+  //   { name: "stop 3", address: "2003 abc st", tel: "0805550003" },
+  // ];
+  const [results, setResults] = useState([]);
   //hooks
+  useEffect(() => {
+    async function fetchData() {
+      const queryResults = await query();
+      console.log(queryResults instanceof Array);
+      console.log("query", queryResults);
+      // You can await here
+      setResults(queryResults);
+    }
+    fetchData();
+  }, []);
 
   //handlers
 
   return (
     <ResultsWrapper>
       <p>Results which are wrapped</p>
-      {results.map((val, index) => {
-        console.log(val.name);
-        return <Result key={index} result={val} />;
+      {results.map((val) => {
+        return <Result key={val.SiteId} result={val} />;
       })}
     </ResultsWrapper>
   );
