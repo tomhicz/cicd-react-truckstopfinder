1af02169560eacd5a75890c3838ca360b36eaedd
Tom Hickz <999194+tomhicz@users.noreply.github.com>
2021-10-15 15:15:29 +0900

    added Grommet for styling

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { ResultsWrapper } from "../elements/ResultsWrapper";
+import { Box } from "grommet";
+
 import Result from "./results/Result";
-import { query } from "./network";
+//import { query } from "./network";
 
 export default function Results(props) {
   //state
@@ -16,7 +18,16 @@ export default function Results(props) {
 
   return (
     <ResultsWrapper>
-      <p>Results which are wrapped</p>
+      <Box
+        tag="header"
+        align="center"
+        justify="between"
+        background="light-3"
+        pad={{ vertical: "small", horizontal: "medium" }}
+        elevation="medium"
+      >
+        Results which are wrapped
+      </Box>
       {results.map((val) => {
         return <Result key={val.id} result={val} />;
       })}
